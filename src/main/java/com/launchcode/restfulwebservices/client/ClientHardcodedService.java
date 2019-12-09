10538a7879971d16c93c0d98bd599fd052771bbb
Tracy Huber <tmhuber3@gmail.com>
2019-12-09 16:49:10 -0600

    initial set up of spring part of app

---
+++ src/main/java/com/launchcode/restfulwebservices/client/ClientHardcodedService.java
@@ -0,0 +1,51 @@
+package com.launchcode.restfulwebservices.client;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class ClientHardcodedService {
+	
+	private static List<Client> clients = new ArrayList();
+	private static int idCounter = 0;
+	
+	static {
+		clients.add(new Client(++idCounter, "Tracy Huber", "Purchase", new Date(), false ));
+		clients.add(new Client(++idCounter, "Micky Mouse", "Prospect", new Date(), false ));
+		clients.add(new Client(++idCounter, "Led Zeplin", "Refinance", new Date(), false ));
+		
+	}
+	
+	public List<Client> findAll() {
+		return clients;
+	}
+	
+	public Client deleteById(long id) {
+		Client client = findById(id);
+		
+		if(client==null) return null;
+		
+		if(clients.remove(client)) {
+			return client;
+		}
+		
+		return null;
+	}
+	
+	public Client findById(long id) {
+		for(Client client:clients) {
+			if(client.getId() == id) {
+				return client;
+			}
+		}
+		
+		return null;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/launchcode/restfulwebservices/client/ClientResource.java
@@ -0,0 +1,38 @@
+package com.launchcode.restfulwebservices.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+
+import com.launchcode.restfulwebservices.client.Client;
+
+@RestController
+@CrossOrigin(origins="http://localhost:4200")
+public class ClientResource {
+	
+	@Autowired
+	private ClientHardcodedService clientService;
+	
+	@GetMapping("/users/{username}/clients")
+	public List<Client> getAllClients(@PathVariable String username) {
+		return clientService.findAll();
+	}
+
+	//DELETE /users/{username}/clients/{id}
+	@DeleteMapping("/users/{username}/clients/{id}")
+	public ResponseEntity<Void> deleteClient(@PathVariable String username, @PathVariable long id) {
+		Client client = clientService.deleteById(id);
+		if(client!=null) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	
+}
